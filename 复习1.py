22c4d84ee0eeb741c54a087aefb60daba84a905d
xiaowang <2805528068@qq.com>
2023-09-25 18:59:18 +0800

    基础

--- 复习1.py
+++ 复习1.py
@@ -49,7 +49,7 @@ print("a1的值为: %d"%a1)
 # 3.2 #eval()函数用来执行一个字符串表达式，并返回表达式的值。也可以用于返回数据本身的类型
 # eval()函数
 # 用来执行一个字符串表达式，并返回表达式的值。可以把字符串转化为list,dict ,tuple
-a2 = eval(input("请输入小数："))
+"""a2 = eval(input("请输入小数："))
 print("输出的是数字：%d"%a2)
 a3 = input("请输入字符串：")
 print(f'输出第一个字符：{a3[0]}')
@@ -71,20 +71,130 @@ print('e,f,g的值分别为{}  {}  {}'.format(e,f,g))
 list1 = list(map(eval,input("请输入三个数字用空格隔开").split(" "))) #存入list列表
 for i in list1:
     if i>0:
-        print(i)
+        print(i)"""
+
+
+
+"""
+    4. list 列表
+"""
+"""# 除数字以外其他任何数值都必须要用单引号括起（无论是中文还是其他国家语言，都必须要），而数字数值可以不用单引号括起
+
+# 4.1 列表创建的2种方式
+# （1）list 创建
+list1 = list() #创建空列表
+list1 = list(('nihao',99,99.0)) #***** 注意：在使用 list() 函数创建列表时，一定要注意双括号。
+print(list1)
+list2 = list(map(eval,input("请输入数字用空格隔开来创建列表").split(" "))) # list 加上map创建
+print(list2)
+
+# （2）用[] 创建
+list3 = ["jjj","哈哈哈",88,89.00]
+print(list3)
+
+
+# 4.2 list 的索引   #不包含切片右边的值
+# （1）切片  [strat : end : step]   [strat : end]
+print(list3[1:2])
+print("从第一个开始每隔2个取一个：",list3[0:4:2])
+# （2）下标索引
+print(list3[0])
+# (3)for循环遍历
+# (4) 确定列表中是否存在指定的项 in 关键字   在就返回true 否则false
+def one():
+    if 88 in list3:
+        return print("88在list3中")
+    else:
+        return print("88不在list3中")
+one()
+
+# 4.3 list 的合并和复制
+# （1） +合并
+one = ['你好',999]
+one2 = list(('hh',66))
+one3 = one+one2
+print(one3)
+# （2） 列表的复制 *
+one4 = one*4
+print(one4)
+
+# 4.4 list 的嵌套
+two = [['hh',88,00],'jj',[99,'pp']]
+print(two)
+print(two[0][1:3]) #不包含切片右边的值
+
+# 4.5 列表比较需要引入 operator 模块的 eq 方法。
+
+# 4.6 列表的函数
+# （1） len() 判断长度
+lit1 = list(('hhh',88,9090))
+print(len(lit1))
+# （2） type() 返回变量的类型
+lit2 = list((88,3,4))
+print(type(lit2))"""
+
+
+# 5 集合
+# 5，1 集合的创建
+# （1） set()
+list1 = list(('11',22,33))
+set1 = set(list1)
+print(set1)
+set2 = {99,'hhh'}
+print(set2)
+# (2) {} 创建
+set2 = {1,2,3,4}
+set3 = set(range(1,9,3)) # range 创建的是[] 列表
+
+print('set2为：',set2,'set3为：',set3)
+# 5.2 集合的函数
+#（1） update()方法更新整个元组
+set2.update(set3)
+print('set2为：',set2) # set2与set3并集
+
+# (2) clear()方法用于清空整个集合
+set2.clear()
+print('set2为：',set2)
+
+# (3) 通过discard()方法丢弃指定的元素
+set3.discard(1)
+print('set3为：',set3)
+
+# (4) 通过remove()方法丢弃指定的元素
+set3.discard(4)
+print('set3为：',set3)
+
+# (5) 通过pop()方法弹出最上面的元素 pop 弹出元素之后相当于删除元素
+print('pop函数弹出set3元素：',set3.pop())
+print('set3为：',set3)
+
+# (6)集合增加元素  add()为末尾追加值 ； 通过updata 添加元素
+set3.add(1) #末尾添加
+print('set3为：',set3)
+
+set3.update([1,3,5]) #并集
+print('set3为：',set3)
+
+# (7) 判断元素是否在集合中 in ; not in
+print(8 in set3)
+# （8） 删除整个集合 del; clear() 清除整个集合的元素
+del set3
+set2.clear()
+print('set2为',set2)
+
+
+# 5.3 集合的交集&、并集| 和差集- 数学运算
+set2 = {1,4,6,9}
+set3 = set(range(1,9,2))
+print('set2为：',set2,'set3为：',set3)
+print('set2与set3的交集为：',set2&set3)
+print('set2与set3的并集为：',set2|set3)
+print('set2与set3的差集为：',set2-set3)
 
 
 
 
-
-
-
-
-
-
-
-
-
+# 6.
 
 
 
